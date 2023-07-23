11540e6e95d8ea10aad4471250898dc6ea53b674
PawanBhatt28 <imcool.pawanbhattjarvis@gmail.com>
2023-07-23 20:14:12 +0530

    head movement detection working goood

---
+++ realtime.py
@@ -0,0 +1,33 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+x_vals = []
+y_vals = []
+
+
+def animate(i):
+    data = pd.read_csv("data.csv")
+    x = data["time"][20:]
+    y1 = data["left_eye"][20:]
+    y2 = data["right_eye"][20:]
+    y3 = data["lips"][20:]
+    y4 = data["head_h"][20:]
+    y5 = data["head_v"][20:]
+    y6 = data["head_slope"][20:]
+    plt.cla()
+
+   # plt.plot(x, y4)
+   # plt.plot(x, y5)
+
+  #  plt.plot(x, y5)
+    plt.plot(x, y6)
+    plt.legend(["head total", "head slope"])
+    plt.xlabel("Time")
+    
+    
+
+ani = FuncAnimation(plt.gcf(), animate, interval=0.01)
+plt.show()
+
