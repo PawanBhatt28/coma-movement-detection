e3a03dbd7405a00d39a85434aece2dd08b3f9936
PawanBhatt28 <imcool.pawanbhattjarvis@gmail.com>
2023-08-07 23:07:57 +0530

    different threshold

--- realtime.py
+++ realtime.py
@@ -7,30 +7,53 @@ x_vals = []
 y_vals = []
 
 
-def animate(i, count):
+def animate(i):
+    points = -250
+    
     data = pd.read_csv("data.csv")
-    time = data["time"][count:]
-    lefteye = data["left_eye"][20:]
-    righteye = data["right_eye"][20:]
-    lips = data["lips"][20:]
-    headh = data["head_h"][20:]
-    headv = data["head_v"][20:]
-    headslope = data["head_slope"][count:]
-    plt.cla()
+    time = data["time"][points:]
+    lefteye = data["left_eye"][points:]
+  #  righteye = data["right_eye"][points:]
+    lips = data["lips"][points:]
+    headh = data["head_h"][points:]
+    headv = data["head_v"][points:]
+    headslope = data["head_slope"][points:]
+    sin = data["planar_angle"][points:]
 
+ #   ax.clear()
+    ax[0,0].clear()
+    ax[0,0].plot(time, headslope, 'tab:orange')
+    ax[0,0].set_title("Vertical + Horizontal Head")
+
+    ax[0,1].clear()
+    ax[0,1].plot(time, sin, 'tab:red')
+    ax[0,1].set_title("Angular Rotation")
+
+    ax[1,0].clear()
+    ax[1,0].plot(time, headh, 'tab:green')
+    ax[1,0].set_title("Head Horizontal")
+
+    ax[1,1].clear()
+    ax[1,1].plot(time, headv)
+    ax[1,1].set_title("Head Vertical")
+
+   # ax[0,0].plot(time, headslope)
+ #   ax.plot(time, headh)
+ #   ax.plot(time, headv)
+
+
+ #   ax.scatter(time, headslope)
+#    ax.legend(["horizontal", "vertical"])
     
-    #plt.plot(time, headh, color="green")
-   # plt.plot(time, headv, color="red")
-  #  plt.legend(["horizontal", "vertical"])
+   # ax.set_ylim([-5,20])
+  #  ax.set_xlabel("Time")
 
-    plt.scatter(time, headslope, s=5)
-    plt.xlabel("Time")
-    count += 100
-    print(count)
-    
+fig = plt.figure()
+#ax = fig.add_subplot(111)
+fg, ax = plt.subplots(2,2)
 
-count = 20
-ani = FuncAnimation(plt.gcf(), animate, interval=0, repeat=False, frames=500, fargs=(count,))
+
+ani = FuncAnimation(plt.gcf(), animate, interval=100)
 plt.tight_layout()
 plt.show()
 
