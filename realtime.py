538fafb9ce6b7884a88ebf5e774904b7224c6538
PawanBhatt28 <imcool.pawanbhattjarvis@gmail.com>
2023-08-13 00:07:43 +0530

    all feature separately movement change check,!e_eye

--- realtime.py
+++ realtime.py
@@ -20,23 +20,24 @@ def animate(i):
     headslope = data["head_slope"][points:]
     sin = data["planar_angle"][points:]
     abs_mov = data["abs_mov"][points:]
+    lip_sum = data["lip_sum"][points:]
+    lip_ratio = data["lip_ratio"][points:]
 
- #   ax.clear()
     ax[0,0].clear()
     ax[0,0].plot(time, abs_mov, 'tab:orange')
     ax[0,0].set_title("Absolute Movement")
 
     ax[0,1].clear()
-    ax[0,1].plot(time, sin, 'tab:red')
-    ax[0,1].set_title("Angular Rotation")
+    ax[0,1].plot(time, lefteye, 'tab:red')
+    ax[0,1].set_title("Left Eye")
 
     ax[1,0].clear()
-    ax[1,0].plot(time, headh, 'tab:green')
-    ax[1,0].set_title("Head Horizontal")
+    ax[1,0].plot(time, lip_sum, 'tab:green')
+    ax[1,0].set_title("Lips Sum")
 
     ax[1,1].clear()
-    ax[1,1].plot(time, headv)
-    ax[1,1].set_title("Head Vertical")
+    ax[1,1].plot(time, lip_ratio)
+    ax[1,1].set_title("Lips Ratio")
 
    # ax[0,0].plot(time, headslope)
  #   ax.plot(time, headh)
@@ -49,12 +50,11 @@ def animate(i):
    # ax.set_ylim([-5,20])
   #  ax.set_xlabel("Time")
 
-fig = plt.figure()
+
 #ax = fig.add_subplot(111)
 fg, ax = plt.subplots(2,2)
 
-
-ani = FuncAnimation(plt.gcf(), animate, interval=100)
+ani = FuncAnimation(plt.gcf(), animate, interval=10)
 plt.tight_layout()
 plt.show()
 
